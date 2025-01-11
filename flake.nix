f923224ab3c4d849a9234e068beaa52029ddb7cc
Michalis Pardalos <me@mpardalos.com>
2025-01-11 15:57:22 +0000

    Use deploy-rs for deploying remotely

--- flake.nix
+++ flake.nix
@@ -1,10 +1,13 @@
 {
     inputs = {
         nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
-        quadlet-nix.url = "github:SEIAROTg/quadlet-nix";
-        quadlet-nix.inputs.nixpkgs.follows = "nixpkgs";
+        quadlet-nix = {
+            url = "github:SEIAROTg/quadlet-nix";
+            inputs.nixpkgs.follows = "nixpkgs";
+        };
+        deploy-rs.url = "github:serokell/deploy-rs";
     };
-    outputs = { nixpkgs, quadlet-nix, ... }@attrs: {
+    outputs = { self, nixpkgs, quadlet-nix, deploy-rs, ... }@attrs: {
         nixosConfigurations.nixie = nixpkgs.lib.nixosSystem {
             system = "x86_64-linux";
             modules = [
@@ -12,5 +15,24 @@
                 quadlet-nix.nixosModules.quadlet
             ];
         };
+
+        deploy.nodes.nixie = {
+            hostname = "nixie.home.mpardalos.com";
+            profiles.system = {
+                user = "root";
+                path = deploy-rs.lib.x86_64-linux.activate.nixos self.nixosConfigurations.nixie;
+                interactiveSudo = true;
+            };
+        };
+
+        checks = builtins.mapAttrs (system: deployLib: deployLib.deployChecks self.deploy) deploy-rs.lib;
+
+        devShells."x86_64-linux".default =
+            let pkgs = nixpkgs.legacyPackages."x86_64-linux";
+            in pkgs.mkShell {
+                packages = [
+                    deploy-rs.packages."x86_64-linux".default
+                ];
+            };
     };
 }
