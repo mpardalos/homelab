54b33a3dcb948fb80e36812a02f54317ca0b42dc
Michalis Pardalos <me@mpardalos.com>
2025-01-11 16:00:52 +0000

    Format with nixfmt

--- flake.nix
+++ flake.nix
@@ -1,38 +1,49 @@
 {
-    inputs = {
-        nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
-        quadlet-nix = {
-            url = "github:SEIAROTg/quadlet-nix";
-            inputs.nixpkgs.follows = "nixpkgs";
-        };
-        deploy-rs.url = "github:serokell/deploy-rs";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    quadlet-nix = {
+      url = "github:SEIAROTg/quadlet-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
-    outputs = { self, nixpkgs, quadlet-nix, deploy-rs, ... }@attrs: {
-        nixosConfigurations.nixie = nixpkgs.lib.nixosSystem {
-            system = "x86_64-linux";
-            modules = [
-                ./nixie/configuration.nix
-                quadlet-nix.nixosModules.quadlet
-            ];
+    deploy-rs.url = "github:serokell/deploy-rs";
+  };
+  outputs =
+    {
+      self,
+      nixpkgs,
+      quadlet-nix,
+      deploy-rs,
+      ...
+    }@attrs:
+    {
+      nixosConfigurations.nixie = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./nixie/configuration.nix
+          quadlet-nix.nixosModules.quadlet
+        ];
+      };
+
+      deploy.nodes.nixie = {
+        hostname = "nixie.home.mpardalos.com";
+        profiles.system = {
+          user = "root";
+          path = deploy-rs.lib.x86_64-linux.activate.nixos self.nixosConfigurations.nixie;
+          interactiveSudo = true;
         };
+      };
 
-        deploy.nodes.nixie = {
-            hostname = "nixie.home.mpardalos.com";
-            profiles.system = {
-                user = "root";
-                path = deploy-rs.lib.x86_64-linux.activate.nixos self.nixosConfigurations.nixie;
-                interactiveSudo = true;
-            };
+      checks = builtins.mapAttrs (system: deployLib: deployLib.deployChecks self.deploy) deploy-rs.lib;
+
+      devShells."x86_64-linux".default =
+        let
+          pkgs = nixpkgs.legacyPackages."x86_64-linux";
+        in
+        pkgs.mkShell {
+          packages = [
+            deploy-rs.packages."x86_64-linux".default
+            pkgs.nixfmt-rfc-style
+          ];
         };
-
-        checks = builtins.mapAttrs (system: deployLib: deployLib.deployChecks self.deploy) deploy-rs.lib;
-
-        devShells."x86_64-linux".default =
-            let pkgs = nixpkgs.legacyPackages."x86_64-linux";
-            in pkgs.mkShell {
-                packages = [
-                    deploy-rs.packages."x86_64-linux".default
-                ];
-            };
     };
 }
