262f3ee580e3f897bf86c01f342eeed7fd691ac9
Michalis Pardalos <me@mpardalos.com>
2025-01-11 19:59:41 +0000

    Add SSH keys for mpardalos user

--- hosts/nixie/default.nix
+++ hosts/nixie/default.nix
@@ -35,6 +35,10 @@
   users.users.mpardalos = {
     isNormalUser = true;
     extraGroups = [ "wheel" ];
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKVsAwlpLoXVkLgCaGHHkSJrrEk7zsnfR3e+9ZbJDCwz mpardalos@odin"
+      "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC0pYgj0FmRWjLuOOXu20/aguNWe8Q7bg7jky95HoN6fI1optrsHVlR7iEqB3g6BCBSSBjIxVEsymf3Z0iXaPAf9Y60aUXXQrnEfATcSVn2akBiBkCp8vq/k06hrTavPDbF7BUQaV9VcrTMTEnVbHPzpOCW3wcItQ3j3bvWxBMZicYoMyK3oEnUMLOWLYFaYGTj5cfJD5x8OW8QCvLgYNMa7TATfULvTUjU4RCXpFrRs92lDoo4zwPKzTlF9ie8YDagjZdLmLdrg8nM5duITeOVEXFJ/5DAeUrebuHWR9XiCp0sFUzxxSovMTSW8kglGDNU5GVw1VxPQmgJfnPOVUoJo3v4ZJJxVHGsTFh1M+FHLF2dAB4wks37MkRpd3v8AwkW5DAjEU6MR+CguxpAW+zH+eqJ5Gvus3Jigw3ptHYuAmgZ4tW8mC3AxmZCxN2dBAZlIN6Ub+gM7S4vMClOHxs1oJOYmJUUsuEcxc3u/iTjJjR5HAppCwFHugi5AZBGWJ0= mpardalos@magni"
+    ];
   };
 
   environment.systemPackages = with pkgs; [
