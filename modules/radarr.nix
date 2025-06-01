265dd5afd492b881d6fea791b8a4e0eefc92f72a
Michalis Pardalos <me@mpardalos.com>
2025-06-01 13:30:34 +0100

    Use NixOS module for radarr

--- modules/radarr.nix
+++ modules/radarr.nix
@@ -8,26 +8,12 @@
   config =
     let
       cfg = config.my-services.radarr;
-      port = 7878;
+      port = config.services.radarr.settings.server.port;
       service-name = "radarr";
       my-url = config.my-services.reverse-proxy.services.${service-name}.url;
     in
     lib.mkIf cfg.enable {
-      virtualisation.containers.enable = true;
-      virtualisation.podman.enable = true;
-
-      virtualisation.quadlet.containers.${service-name}.containerConfig = {
-        image = "lscr.io/linuxserver/radarr:latest";
-        autoUpdate = "registry";
-        publishPorts = [
-          "127.0.0.1:${toString port}:${toString port}"
-        ];
-        volumes = [
-          "${config.my-services.datadir}:/data"
-          "${service-name}-config:/config"
-        ];
-        environments = config.my-services.container-env // config.my-services.linuxserver-container-env;
-      };
+      services.radarr.enable = true;
 
       my-services.reverse-proxy.services = {
         ${service-name}.port = port;
